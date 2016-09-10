2bcb0df85b9ed16039640ae0086f341048fb4077
Casey Runnells <crunnells@gmail.com>
2016-09-10 10:37:08 -0700

    added hosted jquery script

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
-$(document).ready();
-
 var dialog = document.querySelector('dialog');
 var showDialogButton = document.querySelector('#show-dialog');
-if (!dialog.showModal) {
-    dialogPolyfill.registerDialog(dialog);
-}
-showDialogButton.addEventListener('click', function() {
-    dialog.showModal();
-});
-dialog.querySelector('.close').addEventListener('click', function() {
-    dialog.close();
+
+$(function(){
+  if (!dialog.showModal) {
+      dialogPolyfill.registerDialog(dialog);
+  }
+  showDialogButton.addEventListener('click', function() {
+      dialog.showModal();
+  });
+  dialog.querySelector('.close').addEventListener('click', function() {
+      dialog.close();
+  });
 });
